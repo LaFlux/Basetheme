dba75f91ea0b0340f847907d597c77441a60e3e8
Jinto Antony <jinto1729@gmail.com>
2017-02-07 14:12:11 +0530

    Route updated

--- src/Routes/website.php
+++ src/Routes/website.php
@@ -1,10 +1,14 @@
 <?php
 
+
+Route::group(['middleware' => ['web', 'auth:web']], function () {
     Route::get('/', [
         'name' => 'Home Page',
         'as' => 'extensionsvalley.web.home',
         'uses' => 'ExtensionsValley\Basetheme\WebSiteController@getIndex',
     ]);
+});
+
 Route::group(['middleware' => 'web'], function () {
     Route::get('/submitcontact', [
         'name' => 'Contact Form',
@@ -12,7 +16,8 @@ Route::group(['middleware' => 'web'], function () {
         'uses' => 'ExtensionsValley\Basetheme\WebSiteController@SubmitContactform',
     ]);
 });
-    if(file_exists(base_path('routes/web.php'))){
-        require base_path('routes/web.php');
-    }
+
+if (file_exists(base_path('routes/web.php'))) {
+    require base_path('routes/web.php');
+}
 
